2c9ff9bcada013925af47eef20e1b37c1cf1468e
HoussemCharf <houssem.charf@gmail.com>
2017-03-21 21:26:23 +0100

    minor change concerning the code moving it self

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-# array of files ONLY no Folders included
+# array of files ONLY no Folders included 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 # parsing all files in the directory
 for filename in files:
@@ -8,8 +8,10 @@ for filename in files:
 	file,extension = os.path.splitext(filename)
 	#getting rid of . in the beggining of string
 	extension = extension[1:]
-	#checking if the Folder made with extension exist and creating it if it doesnt exist
-	if (not(os.path.isdir(os.getcwd()+'\\'+extension.upper()))):
-		os.makedirs(os.getcwd()+'\\'+extension.upper())
-	# moving the file
-	shutil.move(os.getcwd()+'\\'+filename,os.getcwd()+'\\'+extension.upper()+'\\'+filename)
+	# trying not to move the script to a folder
+	if (not(filename=="code.py")):
+		#checking if the Folder made with extension exist and creating it if it doesnt exist
+		if (not(os.path.isdir(os.getcwd()+'\\'+extension.upper()))):
+			os.makedirs(os.getcwd()+'\\'+extension.upper())
+		# moving the file
+		shutil.move(os.getcwd()+'\\'+filename,os.getcwd()+'\\'+extension.upper()+'\\'+filename)
