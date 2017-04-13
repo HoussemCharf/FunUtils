05f8f3b3d1f27b08964ff7c8096f83d7748ed3a1
HoussemCharf <houssem.charf@gmail.com>
2017-04-13 00:28:33 +0000

    Proxy grabber gonna add a check dead or alive functionality later too tired and exhausted to bother now -_-

---
+++ newproxy.py
@@ -0,0 +1,22 @@
+
+# Importing needed libs
+import httplib
+# importing beautiful soup for parsing html tags
+from bs4 import BeautifulSoup as BS
+# setting up the connecting using get request
+conn = httplib.HTTPSConnection("www.sslproxies.org")
+conn.request("GET", "/")
+# getting the response and storing it in data
+response = conn.getresponse()
+data = response.read()
+# applying beautifulsoup for parsing
+soup = BS(data)
+# parsing the table for the needed infos
+table = soup.find('tbody')
+rows = table.findAll('tr')
+for tr in rows:
+	cols = tr.findAll('td')
+	# parsing and storing data in each row
+	IP_Address,Port,Code_Country,Country,Type_proxy,Google,https,LastCheck = [c.text for c in cols]
+	# displaying string along with needed infos
+	print IP_Address+" "+Port+" "+Country+" "+Type_proxy
