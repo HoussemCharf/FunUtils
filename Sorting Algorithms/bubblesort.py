983518aff89dc173640a87c0f5e68131b74e8610
Sumit Jain <42279031+sumitjain0695@users.noreply.github.com>
2019-10-15 03:03:14 +0530

    modified bubble sort

--- Sorting Algorithms/bubblesort.py
+++ Sorting Algorithms/bubblesort.py
@@ -1,20 +1,29 @@
-def bubbleSort(arr):
+def bubble_sort(arr, simulation=False):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
     n = len(arr)
- 
-    # Traverse through all array elements
-    for i in range(n):
- 
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    swapped = True
+    
+    iteration = 0
+    if simulation:
+        print("iteration",iteration,":",*arr)
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+                if simulation:
+                    iteration = iteration + 1
+                    print("iteration",iteration,":",*arr)
+                    
+    return arr
  
 # Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = [int(x) for x in input().split()]
  
 bubbleSort(arr)
  
