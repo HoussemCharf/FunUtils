d726e6e7169c703b569c146ae2a93cf1af35457b
Sayan Majhi <sayan2044@gmail.com>
2019-10-25 14:22:17 +0530

    Update selectionsort.py

--- Sorting Algorithms/selectionsort.py
+++ Sorting Algorithms/selectionsort.py
@@ -2,6 +2,7 @@ def selection_sort(alist):
     for i in range(0, len(alist) - 1):
         smallest = i
         for j in range(i + 1, len(alist)):
+            if alist[j] < alist[smallest]:
                 smallest = j
         alist[i], alist[smallest] = alist[smallest], alist[i]
  
